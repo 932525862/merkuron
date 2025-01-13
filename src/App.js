005b5f62a859b6e459a7075355f88ccbad07686a
muhammadyusuf <muhammadyusuf@kali.muhammadyusuf>
2025-01-13 23:03:01 +0500

    eng ohiri

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React from "react";
 import { Layout } from "antd";
+import img from "./img/Система-качества.webp"
 import "./App.css";
-import Sertf from "./img/merkuron.jpg";
-// import Protokol from "./img/merkuron-protokol.jpg";
 
 const { Content } = Layout;
 
 function App() {
-  const redirectToSite = (url) => {
-    window.location.href = url;
-  };
-
   const openGoogleImageSearch = (imageSrc) => {
     const googleSearchUrl = `https://www.google.com/searchbyimage?image_url=${encodeURIComponent(imageSrc)}`;
     window.open(googleSearchUrl, "_blank");
@@ -34,53 +29,23 @@ function App() {
             gap: "80px",
           }}
         >
-          {/* Sertifikat rasmini joylash */}
-          {/* <div className="image-frame">
-          <a href='https://postimg.cc/B8LszS1Y' target='_blank'><img src='https://i.postimg.cc/B8LszS1Y/merkuronpoysk-img.jpg' border='0' alt='merkuronpoysk-img'/></a>         
-         </div>
-           */}
           <div className="image-frame">
-            <a
-              href="https://www.merkuron.com/static/media/%20%D0%A1%D0%B8%D1%81%D1%82%D0%B5%D0%BC%D0%B0-%D0%BA%D0%B0%D1%87%D0%B5%D1%81%D1%82%D0%B2%D0%B0.067e9dea591dd04939dc.jpg"
-              onClick={() => redirectToSite("https://www.merkuron.com/")}
-            >
-              <img
-                src={Sertf}
-                alt="Система - Качества - Федеральная"
-                title="Система - Качества - Федеральная"
-                className="image"
-                onClick={() => openGoogleImageSearch(Sertf)}
-                style={{
-                  cursor: "pointer",
-                }}
-              />
-            </a>
+            <img
+              src={img}
+              alt="Сертификат качества системы Меркурон"
+              title="Сертификат качества системы Меркурон"
+              className="image"
+              onClick={() =>
+                openGoogleImageSearch("https://merkuron.com/static/img/merkuron.jpg")
+              }
+              style={{
+                cursor: "pointer",
+              }}
+            />
             <p style={{ textAlign: "center", marginTop: "10px" }}>
-              Система - Качества - Федеральная
+              Сертификат качества системы Меркурон
             </p>
           </div>
-
-          {/* Protokol rasmini joylash */}
-          {/* <div className="image-frame">
-            <a
-              href="https://www.merkuron.com/static/media/%20%D0%A1%D0%B8%D1%81%D1%82%D0%B5%D0%BC%D0%B0-%D0%BA%D0%B0%D1%87%D0%B5%D1%81%D1%82%D0%B2%D0%B0.067e9dea591dd04939dc.jpg"
-              onClick={() => redirectToSite("https://www.merkuron.com/")}
-            >
-              <img
-                src={Protokol}
-                alt="Система - Качества - Федеральная"
-                title="Система - Качества - Федеральная"
-                className="image"
-                onClick={() => openGoogleImageSearch(Protokol)}
-                style={{
-                  cursor: "pointer",
-                }}
-              />
-            </a>
-            <p style={{ textAlign: "center", marginTop: "10px" }}>
-              Система-Качества-Федеральная
-            </p>
-          </div> */}
         </div>
       </Content>
     </Layout>

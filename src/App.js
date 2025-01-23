34daff0964cdc29f18f3c95a8f45c1defdeb8816
muhammadyusuf <muhammadyusuf@kali.muhammadyusuf>
2025-01-23 21:40:24 +0500

    mmd

--- src/App.js
+++ src/App.js
@@ -1,52 +1,14 @@
-import React from "react";
-import { Layout } from "antd";
-import img from "./img/merkuron.jpg";
+
 import "./App.css";
-
-const { Content } = Layout;
-
+import Merkuron from "./components/merkuron";
 function App() {
-  const openGoogleImageSearch = () => {
-    const googleSearchUrl = `https://www.google.com/searchbyimage?image_url=${encodeURIComponent(window.location.origin + img)}`;
-    window.open(googleSearchUrl, "_blank");
-  };
+  
+  
 
   return (
-    <Layout style={{ minHeight: "102vh", backgroundColor: "#f0f2f5" }}>
-      <Content
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          padding: "20px",
-        }}
-      >
-        <div
-          className="container"
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            gap: "80px",
-          }}
-        >
-          <div className="image-frame">
-            <img
-              src={img}
-              alt="Сертификат качества системы Меркурон"
-              title="Сертификат качества системы Меркурон"
-              className="image"
-              onClick={openGoogleImageSearch}
-              style={{
-                cursor: "pointer",
-              }}
-            />
-            <p style={{ textAlign: "center", marginTop: "10px" }}>
-              Сертификат качества системы Меркурон
-            </p>
-          </div>
-        </div>
-      </Content>
-    </Layout>
+   <div>
+     <Merkuron/>
+   </div>
   );
 }
 
---
+++ src/components/merkuron.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { Layout } from "antd";
+import img from "../img/merkuron.webp";
+import "../App.css";
+
+const { Content } = Layout;
+
+function Merkuron() {
+  const openGoogleImageSearch = () => {
+    const googleSearchUrl = `https://www.google.com/searchbyimage?image_url=${encodeURIComponent(window.location.origin + img)}`;
+    window.open(googleSearchUrl, "_blank");
+  };
+
+  return (
+    <Layout style={{ minHeight: "102vh", backgroundColor: "#f0f2f5" }}>
+      <Content
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          padding: "20px",
+        }}
+      >
+        <div
+          className="container"
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "80px",
+          }}
+        >
+          <div className="image-frame">
+            <img
+              src={img}
+              alt="Сертификат качества системы Меркурон"
+              title="Сертификат качества системы Меркурон"
+              className="image"
+              onClick={openGoogleImageSearch}
+              style={{
+                cursor: "pointer",
+              }}
+            />
+            <p style={{ textAlign: "center", marginTop: "10px" }}>
+              Сертификат качества системы Меркурон
+            </p>
+          </div>
+        </div>
+      </Content>
+    </Layout>
+  );
+}
+
+export default Merkuron;
